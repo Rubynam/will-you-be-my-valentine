665334ad6bac6d4f1d76242c625fdb68ec39d10d
Dai Nam <0961563317nam@gmail.com>
2024-02-14 21:43:20 +0700

    change context

--- src/App.tsx
+++ src/App.tsx
@@ -12,22 +12,17 @@ export default function Page() {
 
   const getNoButtonText = () => {
     const phrases = [
-      "No",
-      "Are you sure?",
-      "What if I asked really nicely?",
-      "Pretty please",
-      "With a chocolate rice cake on top",
-      "What about a matcha frostie",
-      "PLEASE POOKIE",
-      "But :*(",
-      "I am going to die",
-      "Yep im dead",
-      "ok ur talking to nathan's ghost",
-      "please babe",
-      ":((((",
-      "PRETTY PLEASE",
-      "Estoy muerto",
-      "No :(",
+      "Hem được đâu",
+      "Yêu anh phải hem?",
+      "Em bấm nhầm rồi đó",
+      "Bấm Có đi kìa",
+      "Nút này bị hư rồi",
+      "Hem phải đâu",
+      "Đừng mà",
+      "Huhu :*(",
+      "Đừng bấm nữa mà",
+      "Lêu lêu",
+      "T_T",
     ];
 
     return phrases[Math.min(noCount, phrases.length - 1)];
@@ -38,7 +33,7 @@ export default function Page() {
       {yesPressed ? (
         <>
           <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
-          <div className="my-4 text-4xl font-bold">WOOOOOO!!! I love you pookie!! ;))</div>
+          <div className="my-4 text-4xl font-bold">Anh yêu em nhiều lắm ❤️❤️❤️❤️ ;))</div>
         </>
       ) : (
         <>
@@ -53,13 +48,13 @@ export default function Page() {
               style={{ fontSize: yesButtonSize }}
               onClick={() => setYesPressed(true)}
             >
-              Yes
+              Có
             </button>
             <button
               onClick={handleNoClick}
               className=" rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700"
             >
-              {noCount === 0 ? "No" : getNoButtonText()}
+              {noCount === 0 ? "Không" : getNoButtonText()}
             </button>
           </div>
         </>
